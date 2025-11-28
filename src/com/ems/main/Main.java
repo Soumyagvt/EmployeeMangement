1c56aae58a145d7251caa221d1f3e444228ca021
Soumyagvt <soumyagangavathi@gmail.com>
2025-11-28 20:23:28 +0530

    Update Main.java

--- src/com/ems/main/Main.java
+++ src/com/ems/main/Main.java
@@ -2,6 +2,6 @@ package com.ems.main;
 import com.ems.ui.LoginPage;
 public class Main {
     public static void main(String[] args) {
-        new LoginPage().setVisible(true);  // Launches GUI login
+        new LoginPage().setVisible(true);  // Launches GUI login  test for commit
     }
 }
